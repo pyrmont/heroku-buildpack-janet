6d5665a461e7ec86ca0e3fa40d68a98d029fd87f
Michael Camilleri <mike@inqk.net>
2020-10-30 15:02:27 +0900

    Fix conditional in detect script

--- test/detect_test.sh
+++ test/detect_test.sh
@@ -2,15 +2,15 @@
 # file: test/detect_test.sh
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
 
-test_detect_with_project_file() {
+testDetectWithProjectFile() {
   touch ${BUILD_DIR}/project.janet
   capture detect
   assertCapturedSuccess
   assertCaptured "Janet"
 }
 
-# test_detect_with_no_project_file() {
-#   capture detect
-#   assertCapturedError
-#   assertCaptured "no"
-# }
+testDetectWithNoProjectFile() {
+  capture detect
+  assertCapturedError
+  assertCaptured "no"
+}
